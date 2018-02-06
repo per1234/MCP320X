b3b9e81dcf180ac063db8fe6066fd64c86c841c6
Nicolas Gagnon <32691913+nickgagnon@users.noreply.github.com>
2018-02-06 17:40:00 -0500

    Added by Nicolas

---
+++ Arduino_MCP320X_Master/MCP320X.h
@@ -0,0 +1,18 @@
+#ifndef MCP320X_H
+#define MCP320X_H
+
+enum Type {
+  MCP3204,
+  MCP3208
+};
+
+enum InputType {
+  SINGLE,
+  DIFFERENTIAL
+};
+
+void mcp320xInit(InputType readType, Type mcpType, uint16_t slavePin);
+int16_t mcp320xRead(uint8_t channel);
+
+
+#endif
---
+++ Arduino_MCP320X_Master/exemple/MCP3204_Read/MCP3204_Read.ino
@@ -0,0 +1,17 @@
+#include <SPI.h>
+#include "MCP320X.h"
+
+uint8_t channel = 0;
+const int PINSLAVE = 8;
+
+void setup() {
+  Serial.begin(115200);
+  mcp320xInit(SINGLE, MCP3204, PINSLAVE);
+}
+
+void loop() {
+  delay(100);
+  uint16_t value = mcp320xRead(channel);
+  Serial.println(value);
+}
+
